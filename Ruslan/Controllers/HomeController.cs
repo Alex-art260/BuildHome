fb7e853ed53977c634fa4b3aa5872d86a0e097df
UncleAlex777 <a.gospodinov@runetsoft.ru>
2024-07-17 10:37:55 +0300

    initial commit

---
+++ Ruslan/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Mvc;
+using Ruslan.Models;
+using System.Diagnostics;
+
+namespace Ruslan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult AboutCompany()
+        {
+            return View();
+        }
+
+        public IActionResult BuildHouse()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
